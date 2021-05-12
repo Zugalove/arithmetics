59c4f8560fdef528928aeaf2cea3d2821e4688eb
NDYALIVANAZ <ndyalivanaz@cput.ac.za>
2021-05-12 16:38:09 +0200

    first commit

---
+++ src/main/java/Add.java
@@ -0,0 +1,8 @@
+import com.math.arithmetics.DemoApplication;
+
+public class Add  extends DemoApplication {
+
+    public static int  sum(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/main/java/com/math/arithmetics/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.math.arithmetics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+         DemoApplication da=new DemoApplication();
+
+	public DemoApplication getDa() {
+		return da;
+	}
+}
